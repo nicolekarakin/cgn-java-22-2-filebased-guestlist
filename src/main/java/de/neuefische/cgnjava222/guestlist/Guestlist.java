1892459e4ca39d7d27107c97f1da55cf5a3491a9
Daniel Schwarz <daniel.schwarz@neuefische.de>
2022-07-07 08:58:23 +0200

    Write to file system

--- src/main/java/de/neuefische/cgnjava222/guestlist/Guestlist.java
+++ src/main/java/de/neuefische/cgnjava222/guestlist/Guestlist.java
@@ -1,7 +1,11 @@
 package de.neuefische.cgnjava222.guestlist;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Guestlist {
 
@@ -11,7 +15,9 @@ public class Guestlist {
         return guests;
     }
 
-    public void setGuests(List<String> guests) {
+    public void setGuests(List<String> guests) throws IOException {
         this.guests = guests;
+        String fileContent = String.join("\n", guests) + "\n";
+        Files.writeString(Paths.get("guests.txt"), fileContent);
     }
 }
--- src/test/java/de/neuefische/cgnjava222/guestlist/GuestlistTest.java
+++ src/test/java/de/neuefische/cgnjava222/guestlist/GuestlistTest.java
@@ -3,6 +3,9 @@ package de.neuefische.cgnjava222.guestlist;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
 
@@ -16,10 +19,27 @@ public class GuestlistTest {
     }
 
     @Test
-    void shouldReadSameGuestsAsWrittenBefore() {
+    void shouldReadSameGuestsAsWrittenBefore() throws IOException {
         Guestlist guestlist = new Guestlist();
         guestlist.setGuests(List.of("Karl", "Ute"));
         List<String> actual = guestlist.getGuests();
         Assertions.assertEquals(List.of("Karl", "Ute"), actual);
+    }
+
+    @Test
+    void shouldWriteToFileSystem() throws IOException {
+        // given
+        Guestlist guestlist = new Guestlist();
+
+        // when
+        guestlist.setGuests(List.of("Theodor", "Anette"));
+
+        // then
+        String actual = Files.readString(Path.of("guests.txt"));
+        Assertions.assertEquals(
+                """
+                        Theodor
+                        Anette
+                        """, actual);
     }
 }
